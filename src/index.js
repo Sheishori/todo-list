a5b97b9f09a7e0b02362c97e8de11300b86cd3c0
Sheishori <sheishori@gmail.com>
2022-07-19 13:03:30 +0200

    Generate project list from the array

--- src/index.js
+++ src/index.js
@@ -19,3 +19,33 @@ const allProjects = (() => {
 const project = (name, color) => {
 	return {name, color};
 };
+
+const projectsList = document.querySelector("#projects");
+
+function updateProjectsList() {
+	projectsList.textContent = "";
+	let projects = allProjects.getProjects();
+	for (let project in projects) {
+		generateProject(projects[project]);
+	};
+};
+
+function generateProject(project) {
+	let li = document.createElement("li");
+	let img = document.createElement("img");
+	let name = document.createElement("div");
+	img.src = "./list.png";
+	img.alt = "📝";
+	name.textContent = project.name;
+	name.classList.add("list");
+
+	li.append(img, name);
+	projectsList.appendChild(li);
+};
+
+let list1 = project("Home", "gray");
+let list2 = project("Work", "gray");
+allProjects.addProject(list1);
+allProjects.addProject(list2);
+
+updateProjectsList();
