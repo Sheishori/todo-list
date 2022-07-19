0c7a1354a38c839455af9b0a2df8184e73198445
Sheishori <sheishori@gmail.com>
2022-07-19 12:31:30 +0200

    Add a factory function to make new projects and a module to hold them

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import { addTask } from "./addTask";
 
 addTask();
+
+const allProjects = (() => {
+	let projects = [];
+
+	function getProjects() {
+		return projects;
+	};
+
+	function addProject(newProject) {
+		projects.push(newProject);
+	};
+
+	return {getProjects, addProject};
+})();
+
+const project = (name, color) => {
+	return {name, color};
+};
